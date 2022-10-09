1f1c092ded16a57fbbde20be535287bd8bf90dfa
AngeloC94 <tripway7777@gmail.com>
2022-10-09 15:04:08 +0200

    props 03 done

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import { Welcome } from "./Welcome"
 export class App extends React.Component {
     render() {
         return <div><Hello />
-            <Welcome />
+            <Welcome age='77' />
         </div>
     }
 }
--- src/Welcome.js
+++ src/Welcome.js
@@ -2,8 +2,11 @@ import React from "react";
 
 
 export class Welcome extends React.Component {
-    render(){
-        return <p>Welcome, {this.props.name}!</p>
+    render() {
+        return <>
+            <p>Welcome, {this.props.name}!</p>
+            <p>Your age is, {this.props.age}!</p>
+        </>
     }
 }
 
