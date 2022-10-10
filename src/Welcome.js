769583e02d69e71462f39f9d308d7e6fc12f4b99
AngeloC94 <tripway7777@gmail.com>
2022-10-10 12:23:47 +0200

    conditional rendering 01 done

--- src/Welcome.js
+++ src/Welcome.js
@@ -2,11 +2,13 @@ import React from "react";
 import { Age } from "./Age";
 
 export class Welcome extends React.Component {
-    render() {
-        return <>
-        <p>Welcome, {this.props.name}!</p>
-        <Age age={77}/>
+    render(){
+        return (
+        <>
+            <p>Welcome, {this.props.name}!</p>
+            {this.props.age > 18 && <Age age= {this.props.age}/>}
         </>
+        )
     }
 }
 
