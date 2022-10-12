207f452cf2e5dc2d711b4f76543ffc21002734cb
AngeloC94 <tripway7777@gmail.com>
2022-10-12 11:09:12 +0200

    component lifecycle 01 done

--- src/Counter.js
+++ src/Counter.js
@@ -1,22 +1,22 @@
-import React from 'react';
-import { CounterDisplay } from './CounterDisplay';
+import React from "react";
+import { CounterDisplay } from "./CounterDisplay";
+
 export class Counter extends React.Component {
     state = { count: this.props.initialValue };
 
-	constructor(props) {
-		super(props);
-
+	componentDidMount() {
 		setInterval(() => {
 			this.setState({
-				count: this.state.count + this.props.incrementAmount
+				count: this.state.count + this.props.incrementAmount 
 			});
 		}, this.props.incrementInterval);
 	}
 
 	render() {
-		return (
-			<CounterDisplay displayCounter={this.state.count}/>
+		return (			
+				<CounterDisplay displayCounter={this.state.count} />			
 		);
 	}
 }
 
+// con il constructor abbiamo una doppia renderizzazione
