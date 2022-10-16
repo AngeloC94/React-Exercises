ba9b7968417c2d1e21fa004c5052ace87688f2bf
AngeloC94 <tripway7777@gmail.com>
2022-10-16 12:36:57 +0200

    forms 03 done

--- src/Login.js
+++ src/Login.js
@@ -1,11 +1,12 @@
 import React from "react"
 
 
-export class Login extends React.Component{
-    state= {
+export class Login extends React.Component {
+    state = {
         username: "",
         password: "",
-        remember: false
+        remember: false,
+        disabled: true
     }
     handleInputEvent = (event) => {
         const value = event.target.value
@@ -13,14 +14,24 @@ export class Login extends React.Component{
         const type = event.target.type
         const checked = event.target.checked
         this.setState({
-            [name] : type === "checkbox" ? checked : value
+            [name]: type === "checkbox" ? checked : value
+        })
+
+
+    }
+
+    onLogin = (e) => {
+        const verify = e.target.disabled
+        this.setState({
+            disabled: verify
         })
     }
     render() {
-        return  <div>
-                    <input name="username" value={this.state.username} onChange={this.handleInputEvent} />
-                    <input name="password" type="password" value={this.state.password} onChange={this.handleInputEvent} />
-                    <input name="remember" type="checkbox" checked={this.state.remember} onChange={this.handleInputEvent} />
-                </div>
+        return <div>
+            <input name="username" value={this.state.username} onChange={this.handleInputEvent} />
+            <input name="password" type="password" value={this.state.password} onChange={this.handleInputEvent} />
+            <input name="remember" type="checkbox" checked={this.state.remember} onChange={this.handleInputEvent} />
+            <button disabled={!this.state.username || !this.state.password} onClick={this.onLogin}>Login</button>
+        </div>
     }
 }
