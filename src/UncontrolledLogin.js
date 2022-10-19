700e73439979c0269ef5eb03f524c52db4ca9022
AngeloC94 <tripway7777@gmail.com>
2022-10-19 18:51:38 +0200

    forms 06 done

--- src/UncontrolledLogin.js
+++ src/UncontrolledLogin.js
@@ -1,8 +1,10 @@
-import React from "react"
+import React, { createRef } from "react"
 
 
-export class UncontrolledLogin extends React.Component{
-    handleSubmit= (e) => {
+export class UncontrolledLogin extends React.Component {
+    _formRef = createRef();
+
+    handleSubmit = (e) => {
         e.preventDefault()
 
         const username = e.target.elements.username.value
@@ -17,14 +19,14 @@ export class UncontrolledLogin extends React.Component{
     }
 
     render() {
-        return  <div>
-                    <form onSubmit={this.handleSubmit}>
-                        <input autoFocus name="username" required/>
-                        <input name="password" type="password" required/>
-                        <input name="remember" type="checkbox" />
-                        <button type="submit" >Login</button>
-                        <button type="reset">Reset</button>
-                    </form>
-                </div>
+        return <div>
+            <form ref={this._formRef} onSubmit={this.handleSubmit}>
+                <input autoFocus name="username" required />
+                <input name="password" type="password" required />
+                <input name="remember" type="checkbox" />
+                <button type="submit" >Login</button>
+                <button type="reset">Reset</button>
+            </form>
+        </div>
     }
 }
