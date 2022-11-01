357031adc53ff3ddeeaf2faadf35723360fe0775
AngeloC94 <tripway7777@gmail.com>
2022-11-01 12:40:19 +0100

    useCallback done

--- src/useCounter.js
+++ src/useCounter.js
@@ -1,37 +1,37 @@
-import { useState } from "react"
-function useCounter(){
-    const [counter, setCounter] = useState(0)
+import { useCallback, useState } from "react";
 
-    function handleIncrement(){
-        setCounter((c) => c + 1)
-    }
+function useCounter() {
+  const [counter, setCounter] = useState(0);
 
-    function handleDecrement(){
-        setCounter((c) => c - 1)
-    }
+  const handleIncrement = useCallback(function handleIncrement() {
+    setCounter((c) => c + 1);
+  }, []);
 
-    function resetButton() {
-        setCounter(0)
-    }
+  const handleDecrement = useCallback(function handleDecrement() {
+    setCounter((c) => c - 1);
+  }, []);
 
-    return {
-        counter: counter,
-        onIncrement: handleIncrement,
-        onDecrement: handleDecrement,
-        onReset: resetButton
-    }
+  const resetButton = useCallback(function resetButton() {
+    setCounter(0);
+  }, []);
+
+  return {
+    counter: counter,
+    onIncrement: handleIncrement,
+    onDecrement: handleDecrement,
+    onReset: resetButton,
+  };
 }
 
-export default function CustomCounter(){
+export default function CustomCounter() {
+  const { counter, onIncrement, onDecrement, onReset } = useCounter();
 
-    const {counter, onIncrement, onDecrement, onReset} = useCounter()
-
-    return (
-        <div style={{display: 'flex'}} >
-            <button onClick={onDecrement} >-</button>
-            <h1 >{counter}</h1>
-            <button onClick={onIncrement} >+</button>
-            <button onClick={onReset} >RESET</button>
-        </div>
-    )
+  return (
+    <div style={{ display: "flex" }}>
+      <button onClick={onDecrement}>-</button>
+      <h1>{counter}</h1>
+      <button onClick={onIncrement}>+</button>
+      <button onClick={onReset}>RESET</button>
+    </div>
+  );
 }
